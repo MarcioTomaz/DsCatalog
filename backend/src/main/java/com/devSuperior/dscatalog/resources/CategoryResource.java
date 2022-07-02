2b7f1acd9f8f9efa997e9869834cd977fb1dd82c
MarcioTomaz <marciotmaz@gmail.com>
2022-07-02 17:07:50 -0300

    add: h2 database

--- backend/src/main/java/com/devSuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devSuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.devSuperior.dscatalog.resources;
 
+import com.devSuperior.dscatalog.Services.CategoryService;
 import com.devSuperior.dscatalog.entities.Category;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,13 @@ import java.util.List;
 public class CategoryResource {
 
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public ResponseEntity<List<Category>>findAll(){
 
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L,"books"));
-        list.add(new Category(2L,"Eletronics"));
+        List<Category> list = categoryService.findALl();
 
         return ResponseEntity.ok().body(list);
     }
