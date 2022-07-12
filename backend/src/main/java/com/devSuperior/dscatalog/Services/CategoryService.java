2e50caaa178a1eaf257b9afef2b0c5b2beab5838
MarcioTomaz <marciotmaz@gmail.com>
2022-07-12 13:57:26 -0300

    add:Category by id,  exception handler

--- backend/src/main/java/com/devSuperior/dscatalog/Services/CategoryService.java
+++ backend/src/main/java/com/devSuperior/dscatalog/Services/CategoryService.java
@@ -1,14 +1,15 @@
 package com.devSuperior.dscatalog.Services;
 
 import com.devSuperior.dscatalog.DTO.CategoryDTO;
+import com.devSuperior.dscatalog.Services.exceptions.ResourceNotFoundException;
 import com.devSuperior.dscatalog.entities.Category;
 import com.devSuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +23,14 @@ public class CategoryService {
         List<Category> result = categoryRepository.findAll();
 
         return result.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    };
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity = obj.orElseThrow( () -> new ResourceNotFoundException("Entity not found"));
+
+        return new CategoryDTO(entity);
     };
 }
--- backend/src/main/java/com/devSuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devSuperior/dscatalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import com.devSuperior.dscatalog.entities.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +27,13 @@ public class CategoryResource {
         List<CategoryDTO> list = categoryService.findALl();
 
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity <CategoryDTO> findById(@PathVariable Long id){
+
+        CategoryDTO dto = categoryService.findById(id);
+
+        return ResponseEntity.ok().body(dto);
     }
 }
