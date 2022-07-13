2ad20421da2332b8bc9053df0af8a57aee89b62c
MarcioTomaz <marciotmaz@gmail.com>
2022-07-13 14:41:28 -0300

    add: pagination

--- backend/src/main/java/com/devSuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devSuperior/dscatalog/resources/CategoryResource.java
@@ -4,11 +4,11 @@ import com.devSuperior.dscatalog.DTO.CategoryDTO;
 import com.devSuperior.dscatalog.Services.CategoryService;
 import com.devSuperior.dscatalog.entities.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,9 +22,15 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>>findAll(){
+    public ResponseEntity<Page<CategoryDTO>>findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy){
 
-        List<CategoryDTO> list = categoryService.findALl();
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy );
+
+        Page<CategoryDTO> list = categoryService.findAllPaged(pageRequest);
 
         return ResponseEntity.ok().body(list);
     }
