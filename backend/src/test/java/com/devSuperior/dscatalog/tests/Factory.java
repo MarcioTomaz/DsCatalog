a0215c27c97807ffa31b0cfd16d1118c6e49e55a
MarcioTomaz <marciotmaz@gmail.com>
2022-07-14 15:59:22 -0300

    add: repository tests

---
+++ backend/src/test/java/com/devSuperior/dscatalog/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.devSuperior.dscatalog.tests;
+
+import com.devSuperior.dscatalog.DTO.ProductDTO;
+import com.devSuperior.dscatalog.entities.Category;
+import com.devSuperior.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct(){
+
+        Product product = new Product(1L, "Phone", "Good Phone", 800.0, "www.ghoogle.com", Instant.parse(("2020-10-20T03:00:00Z")));
+        product.getCategories().add(new Category(2L, "Eletronics"));
+        return product;
+    }
+
+    public static ProductDTO createProductDTO(){
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+}
